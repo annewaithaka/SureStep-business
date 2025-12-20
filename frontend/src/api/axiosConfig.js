ec4e014b0bfd95b705ffd08eec9a224092d51c5c
annewaithaka <annewaithaka68@gmail.com>
2025-12-20 12:18:23 +0300

    Add gunicorn for Render

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getToken, removeToken } from "../auth/auth";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
